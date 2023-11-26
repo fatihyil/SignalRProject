17f6a895aaab28b7dcedda8d884bc70bb246cdfa
Fatih YILMAZ <41871038+fatihyil@users.noreply.github.com>
2023-11-26 20:04:22 +0300

    TestimonialController and SocialMediaController Add

---
+++ SignalRApi/Controllers/SocialMediaController.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.SocialMediaDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SocialMediaController : ControllerBase
+    {
+        private readonly ISocialMediaService _socialMediaService;
+        private readonly IMapper _mapper;
+
+        public SocialMediaController(ISocialMediaService socialMediaService, IMapper mapper)
+        {
+            _socialMediaService = socialMediaService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult SocialMediaList()
+        {
+            var socialMedias = _mapper.Map<List<ResultSocialMediaDto>>(_socialMediaService.TGetListAll());
+            return Ok(socialMedias);
+        }
+        [HttpGet("GetSocialMedia")]
+        public IActionResult GetSocialMedia(int id)
+        {
+            var socialMedia = _mapper.Map<GetSocialMediaDto>(_socialMediaService.TGetById(id));
+            return Ok(socialMedia);
+        }
+        [HttpPost]
+        public IActionResult CreateSocialMedia(CreateSocialMediaDto createSocialMediaDto)
+        {
+            _socialMediaService.TAdd(_mapper.Map<SocialMedia>(createSocialMediaDto));
+            return Ok("Sosyal Medya Bilgisi Eklendi");
+        }
+        [HttpPut]
+        public IActionResult UpdateSocialMedia(UpdateSocialMediaDto updateSocialMediaDto)
+        {
+            _socialMediaService.TUpdate(_mapper.Map<SocialMedia>(updateSocialMediaDto));    
+            return Ok("Sosyal Medya Bilgisi Güncellendi");
+        }
+        [HttpDelete]
+        public IActionResult DeleteSocialMedia(int id)
+        {
+            _socialMediaService.TDelete(_socialMediaService.TGetById(id));
+            return Ok("Sosyal Medya Bilgisi Silinmiştir");
+        }
+    }
+}
---
+++ SignalRApi/Controllers/TestimonialController.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.TestimonialDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialController : ControllerBase
+    {
+        private readonly ITestimonialService _testimonialService;
+        private readonly IMapper _mapper;
+
+        public TestimonialController(ITestimonialService testimonialService, IMapper mapper)
+        {
+            _testimonialService = testimonialService;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult TestimonialList() 
+        {
+            var testimonials = _mapper.Map<List<ResultTestimonialDto>>(_testimonialService.TGetListAll());
+            return Ok(testimonials); 
+        }
+        [HttpGet("GetTestimonial")]
+        public IActionResult GetTestimonial(int id) 
+        {
+            var testimonial = _testimonialService.TGetById(id);
+            return Ok(testimonial);
+        }
+        [HttpPost]
+        public IActionResult CreateTestimonial(CreateTestimonialDto createTestimonialDto)
+        {
+            _testimonialService.TAdd(_mapper.Map<Testimonial>(createTestimonialDto));
+            return Ok("Müşteri Yorumu Eklenmiştir");
+        }
+        [HttpPut]
+        public IActionResult UpdateTestimonial(UpdateTestimonialDto updateTestimonialDto)
+        {
+            _testimonialService.TUpdate(_mapper.Map<Testimonial>(updateTestimonialDto));
+            return Ok("Müşteri Yorumu Güncellenmiştir");
+        }
+        [HttpDelete]
+        public IActionResult DeleteTestimonial(int id)
+        {
+            _testimonialService.TDelete(_testimonialService.TGetById(id));
+            return Ok("Müşteri Yorum Bilgisi Silinmiştir");
+        }
+    }
+}
